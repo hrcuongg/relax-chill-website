ff2b5af352ebb51cb0e9c4974ed2462920e83bc6
BinhNguyen <anhdoanso1@gmail.com>
2019-06-24 19:42:02 +0700

    hihi

--- Js/audio.js
+++ Js/audio.js
@@ -1,1 +1,14 @@
+$(function() {
+	$('.back-top').on('click', function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop: 0}, 1000);
+		/* Act on the event */
+	});
+	$('.logovamenu a:nth-child(4) ').on('click', function(event) {
+		event.preventDefault();
+		$('html,body').animate({scrollTop: $('.footer').offset().top}, 900,);
+		/* Act on the event */
+	});;
+});
+
 new WOW().init();
