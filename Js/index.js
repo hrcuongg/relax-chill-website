8354d31f7c90f21c5119399ec34b2493378b3cfd
BinhNguyen <anhdoanso1@gmail.com>
2019-06-24 18:35:11 +0700

    demosanpham

--- Js/index.js
+++ Js/index.js
@@ -10,7 +10,6 @@ $(function() {
 			$('.logovamenu').removeClass('biendoi');
 		}
 	});;
-<<<<<<< HEAD
 	$('.back-top').on('click', function(event) {
 		event.preventDefault();
 		$('html,body').animate({scrollTop: 0}, 1000);
@@ -37,6 +36,4 @@ $(function() {
 		/* Act on the event */
 	});;
 }); 
-=======
-});
->>>>>>> c6d7aeddc7bece8d93be1f25f04dca24b4c296f1
+new WOW().init();
